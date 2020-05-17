3ec32f6c035da4afe8d77ec66f068318128a43b9
carizzuti <carizzuti@student.fullsail.edu>
2020-05-17 01:59:37 -0400

    Create bottom nav bar

--- app/src/main/java/com/example/werankit3/MainActivity.java
+++ app/src/main/java/com/example/werankit3/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.werankit3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.accounts.Account;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialize and Assign Variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //Set home Selected
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.message:
+                        startActivity(new Intent(getApplicationContext(), MessageActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.account:
+                        startActivity(new Intent(getApplicationContext(), AccountActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.createlist:
+                        startActivity(new Intent(getApplicationContext(), CreateListActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.search:
+                        startActivity(new Intent(getApplicationContext(), SearchActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.home:
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
+
--- app/src/main/java/com/example/werankit3/SearchActivity.java
+++ app/src/main/java/com/example/werankit3/SearchActivity.java
@@ -1,8 +1,13 @@
 package com.example.werankit3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -10,5 +15,40 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        //Initialize and Assign Variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //Set home Selected
+        bottomNavigationView.setSelectedItemId(R.id.search);
+
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.message:
+                        startActivity(new Intent(getApplicationContext(), MessageActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.account:
+                        startActivity(new Intent(getApplicationContext(), AccountActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.createlist:
+                        startActivity(new Intent(getApplicationContext(), CreateListActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.search:
+                        return true;
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
