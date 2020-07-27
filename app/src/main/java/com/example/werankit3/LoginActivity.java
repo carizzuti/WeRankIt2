0f4bc11dcf5db2847e876337624d1ad9d3bf700c
carizzuti <carizzuti@student.fullsail.edu>
2020-07-27 16:37:04 -0400

    Firebase User Registration

--- app/src/main/java/com/example/werankit3/LoginActivity.java
+++ app/src/main/java/com/example/werankit3/LoginActivity.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText name;
     private EditText password;
     private Button btnLogin;
+    private TextView userRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +24,19 @@ public class LoginActivity extends AppCompatActivity {
         name = findViewById(R.id.etName);
         password = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        userRegister = findViewById(R.id.tvRegister);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 validate(name.getText().toString(), password.getText().toString());
+            }
+        });
+
+        userRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
     }
--- app/src/main/java/com/example/werankit3/RegisterActivity.java
+++ app/src/main/java/com/example/werankit3/RegisterActivity.java
@@ -1,14 +1,76 @@
 package com.example.werankit3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText userName, userPassword, userEmail;
+    private Button btnRegister;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        userName = findViewById(R.id.etUserName);
+        userPassword = findViewById(R.id.etUserPassword);
+        userEmail = findViewById(R.id.etUserEmail);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validate()) {
+                    // Upload data to the database
+                    String user_email = userEmail.getText().toString().trim();
+                    String user_password = userPassword.getText().toString().trim();
+
+                    firebaseAuth.createUserWithEmailAndPassword(user_email, user_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()) {
+                                Toast.makeText(RegisterActivity.this, "Registration Complete!", Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                            }
+                            else
+                                Toast.makeText(RegisterActivity.this, "Registration Failed", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private boolean validate() {
+        boolean result = false;
+
+        String name = userName.getText().toString();
+        String password = userPassword.getText().toString();
+        String email = userEmail.getText().toString();
+
+        if (name.isEmpty() || password.isEmpty() || email.isEmpty()) {
+            Toast.makeText(this, "Form Incomplete!", Toast.LENGTH_LONG).show();
+        }
+        else
+            result = true;
+
+        return result;
     }
 }
